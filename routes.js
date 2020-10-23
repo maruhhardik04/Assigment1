76828db46fda0cffb42421c59a9742f60531d24a
maruhardik04 <49114615+maruhhardik04@users.noreply.github.com>
2020-10-23 13:20:22 +0530

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,81 @@
+
+
+const requestHandler = (req,res) => {
+    const {url,method,headers} = req;
+
+    res.setHeader('Content-Type','text/html');
+    
+    if(url === '/create-user' && method === 'POST')
+    {
+            const body = [];
+            req.on('data',(chunk)=>{
+                body.push(chunk);
+            });
+
+            return req.on('end',()=>{
+
+                const parseBody  = Buffer.concat(body).toString();
+                console.log(parseBody);
+                res.writeHead(302,{'Location':'/'});
+               return res.end();
+
+            })
+    }
+
+
+    if(url === '/')
+    {
+        res.write(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <title>Document</title>
+        </head>
+        <body>
+                <form action="/create-user" method="post">
+                    <input type="text" name="username">
+                    <button>Submit</button>
+                </form>
+        </body>
+        </html>`);
+        return res.end();
+    }
+
+    
+
+    if(url === '/users')
+    {
+          
+            res.write(`<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <title>Document</title>
+            </head>
+            <body>
+                    <ul>
+                        <li>User1</li>
+                        <li>User2</li>
+                        <li>User3</li>
+                    </ul>
+            </body>
+            </html>`);
+            return res.end();
+    }
+
+    // res.setHeader('Content-Type','text/html');
+        res.write(`<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <title>Document</title>
+        </head>
+        <body>
+               <h1>Hello</h1>
+        </body>
+        </html>`)
+        res.end();
+
+
+
+}
+
+module.exports = requestHandler;
